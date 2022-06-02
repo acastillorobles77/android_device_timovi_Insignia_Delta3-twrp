c4cbb689275ae7f1f95f2603c37801160bf24734
Angel Castillo <49908914+acastillorobles77@users.noreply.github.com>
2022-06-01 21:27:24 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_Insignia_Delta3.mk
+    $(LOCAL_DIR)/omni_Insignia_Delta3.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_Insignia_Delta3-user \
-    twrp_Insignia_Delta3-userdebug \
-    twrp_Insignia_Delta3-eng
+    omni_Insignia_Delta3-user \
+    omni_Insignia_Delta3-userdebug \
+    omni_Insignia_Delta3-eng
